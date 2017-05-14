3a55e83b30fabf9f8d6f8469192b01fe12443fb3
Jonas Holme <joholme92@gmail.com>
2017-05-14 15:29:15 +0200

    Lagt til rette for stoppeklokke-eksprimentering i about.aspx

--- Divingjournal2/Divingjournal2/About.aspx.designer.cs
+++ Divingjournal2/Divingjournal2/About.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace Divingjournal2 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button StartButton;
+        
+        /// <summary>
+        /// Table1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table1;
     }
 }
